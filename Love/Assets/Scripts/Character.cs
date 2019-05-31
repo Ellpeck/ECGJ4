6e44284c41adab6016bfde3d63bcbfa610b255bb
Ellpeck <megamaximal@gmail.com>
2019-05-31 14:15:50 +0200

    prototyping part 1

---
+++ Love/Assets/Scripts/Character.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Character : MonoBehaviour {
+
+    public float speed;
+    public float jumpForce;
+    public Transform groundCheck;
+    public LayerMask groundLayers;
+    public string leftInput;
+    public string rightInput;
+    public string jumpInput;
+
+    private Rigidbody2D body;
+
+    private bool jump;
+    private float horizontalMovement;
+
+    private void Start() {
+        this.body = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        this.horizontalMovement = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(this.jumpInput))
+            this.jump = true;
+    }
+
+    private void FixedUpdate() {
+        this.body.AddForce(new Vector2(this.horizontalMovement * this.speed, 0));
+
+        if (this.jump) {
+            var ground = Physics2D.OverlapCircle(this.groundCheck.position, 0.2F, this.groundLayers);
+            if (ground) {
+                this.body.AddForce(new Vector2(0, this.jumpForce));
+            }
+            this.jump = false;
+        }
+    }
+
+}
---
+++ Tame/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+    public float jumpForce;
+    public Transform groundCheck;
+    public LayerMask groundLayers;
+
+    private Rigidbody2D body;
+
+    private float moveInput;
+    private bool jump;
+
+    private void Start() {
+        this.body = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        this.moveInput = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+            this.jump = true;
+    }
+
+    private void FixedUpdate() {
+        this.body.velocity = new Vector2(this.moveInput * this.speed, this.body.velocity.y);
+
+        if (this.jump) {
+            bool onGround = Physics2D.OverlapCircle(this.groundCheck.position, 0.1F, this.groundLayers);
+            if (onGround) {
+                this.body.velocity = new Vector2(this.body.velocity.x, this.jumpForce);
+            }
+            this.jump = false;
+        }
+    }
+
+}

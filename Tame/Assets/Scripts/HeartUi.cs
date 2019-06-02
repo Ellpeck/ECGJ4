5b63d996cb6653cde1468fc2f2c5d8208b7926d0
Ellpeck <megamaximal@gmail.com>
2019-06-02 18:49:07 +0200

    in game ui

---
+++ Tame/Assets/Scripts/HeartUi.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartUi : MonoBehaviour {
+
+    public Color fullColor;
+    public Color emptyColor;
+    public int index;
+
+    private Health playerHealth;
+    private new Image renderer;
+
+    private bool isFull = true;
+
+    private void Start() {
+        this.playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+        this.renderer = this.GetComponent<Image>();
+    }
+
+    private void Update() {
+        var has = this.playerHealth.currHealth > this.index;
+        if (has != this.isFull) {
+            this.isFull = has;
+            this.renderer.color = this.isFull ? this.fullColor : this.emptyColor;
+        }
+    }
+
+}
---
+++ Tame/Assets/Scripts/PuzzlePieceUi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzlePieceUi : MonoBehaviour {
+
+    public Color gottenColor;
+    public Color notGottenColor;
+    public int index;
+
+    private Player player;
+    private new Image renderer;
+
+    private bool gotten = true;
+
+    private void Start() {
+        this.player = FindObjectOfType<Player>();
+        this.renderer = this.GetComponent<Image>();
+    }
+
+    private void Update() {
+        var has = this.player.hasPuzzlePieces[this.index];
+        if (has != this.gotten) {
+            this.gotten = has;
+            this.renderer.color = this.gotten ? this.gottenColor : this.notGottenColor;
+        }
+    }
+
+}

f230d592eca9e43c36e0655e8388746cac2bc360
Ellpeck <megamaximal@gmail.com>
2019-06-03 14:37:23 +0200

    added buttons and doors

---
+++ Tame/Assets/Scripts/Button.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    private static readonly int Down = Animator.StringToHash("Down");
+    private static readonly int Up = Animator.StringToHash("Up");
+
+    public int requiredHeaviness;
+    public float downTime;
+    public Collider2D topCollider;
+    public Gradient cableOn;
+    public Gradient cableOff;
+    public Door[] doors;
+
+    private Animator animator;
+    private LineRenderer cable;
+    private float downTimer;
+
+    private void Start() {
+        this.animator = this.GetComponent<Animator>();
+        this.cable = this.GetComponent<LineRenderer>();
+    }
+
+    private void Update() {
+        if (this.downTimer > 0) {
+            this.downTimer -= Time.deltaTime;
+            if (this.downTimer <= 0) {
+                this.animator.SetTrigger(Up);
+                this.topCollider.enabled = true;
+                this.cable.colorGradient = this.cableOff;
+                foreach (var door in this.doors)
+                    door.SetPowered(false);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        var player = other.gameObject.GetComponent<Player>();
+        if (player != null && player.heaviness >= this.requiredHeaviness) {
+            this.animator.SetTrigger(Down);
+            this.topCollider.enabled = false;
+            this.cable.colorGradient = this.cableOn;
+            foreach (var door in this.doors)
+                door.SetPowered(true);
+
+            this.downTimer = this.downTime;
+        }
+    }
+
+}
---
+++ Tame/Assets/Scripts/Door.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    private static readonly int Powered = Animator.StringToHash("Powered");
+    private static readonly int Unpowered = Animator.StringToHash("Unpowered");
+
+    private Animator animator;
+    private new Collider2D collider;
+
+    private bool powered;
+
+    private void Start() {
+        this.animator = this.GetComponent<Animator>();
+        this.collider = this.GetComponent<Collider2D>();
+    }
+
+    public void SetPowered(bool powered) {
+        if (powered == this.powered)
+            return;
+        this.powered = powered;
+        this.collider.enabled = !powered;
+        this.animator.SetTrigger(powered ? Powered : Unpowered);
+    }
+
+}
